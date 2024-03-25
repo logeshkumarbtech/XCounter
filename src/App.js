e6b50ee87b814351fb9ce657b03033914c678075
Logesh kumar G <logeshkumarbtech@gmail.com>
2024-03-25 17:40:16 +0530

    Xcounter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [count, setCount] =useState(0);
+
+  const handleInc =(e)=>{
+    setCount((prevCount)=>prevCount+1);
+  } 
+  const handleDec =(e)=>{
+    setCount((prevCount)=>prevCount-1);
+  } 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleInc}>Increment</button>
+      <button onClick={handleDec}>Decrement</button>
     </div>
   );
 }
